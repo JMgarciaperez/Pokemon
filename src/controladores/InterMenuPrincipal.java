ae03dce71c135c08bd45ad4f909f217983640271
Chemichuta <chemichuta1@gmail.com>
2022-05-16 19:40:51 +0200

    configuración interfaz gráfica

---
+++ src/controladores/InterMenuPrincipal.java
@@ -0,0 +1,25 @@
+package controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class InterMenuPrincipal implements Initializable {
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private void empezarPartida(ActionEvent event){
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
--- src/pokemon/ClaseMain.java
+++ src/pokemon/ClaseMain.java
@@ -1,11 +1,40 @@
 package pokemon;
 
+import java.io.IOException;
 import java.sql.*;
 
-public class ClaseMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClaseMain extends Application{
     
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+       
+        @Override
+        public void start(Stage primaryStage)  {
+            
+            try{
+                Parent root = FXMLLoader.load(getClass().getResource(name : "/.vistas/menu principal.fxml"));
+                Scene scene =new Scene(root);
+
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        }
+     
+
+
+
+
+
         Connection conec;
         String url="jdbc:mysql://localhost:3306/pokemones";
         String login="root";
@@ -41,7 +70,7 @@ public class ClaseMain {
         c.addTurnos(t1);
         c.escribirFichero();
     }
- 
+
 
     ClasePokemon.generarPokemonAleatorio(nombre);
 
