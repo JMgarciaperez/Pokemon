d91ab20b9c2180b57b4af066bd6d440833620b7e
JoseAndresCesur <98944622+JoseAndresCesur@users.noreply.github.com>
2022-05-17 13:33:55 +0200

    cambio en interfaz

--- src/controladores/InterMenuPrincipal.java
+++ src/controladores/InterMenuPrincipal.java
@@ -3,6 +3,7 @@ package controladores;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
