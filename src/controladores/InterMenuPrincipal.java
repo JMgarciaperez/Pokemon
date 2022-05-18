0f48527dd30e6fd8c0dc17434cf1758c9558b1a1
JoseAndresCesur <98944622+JoseAndresCesur@users.noreply.github.com>
2022-05-18 20:34:41 +0200

    cambiar de menuPrincipal a la interfaz de elegir

--- src/controladores/InterMenuPrincipal.java
+++ src/controladores/InterMenuPrincipal.java
@@ -1,29 +1,36 @@
 package controladores;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class InterMenuPrincipal implements Initializable {
-  
-    
 
-   
     @FXML
     private Button btnStart;
 
     @FXML
-    private void empezarPartida(ActionEvent event){
-        
-        
-
-        
-
+    private void empezarPartida(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/menuElegir.FXML"));
+        Parent root = loader.load();
+        InterElegir controller = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+        stage.close();
     }
 
     @Override
