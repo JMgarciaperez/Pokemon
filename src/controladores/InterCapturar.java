8e27248c7f86fbbf1c47ba0aff8d28f65d41afeb
JoseAndresCesur <98944622+JoseAndresCesur@users.noreply.github.com>
2022-05-19 10:01:49 +0200

    corregir errores interfaz de capturar

--- src/controladores/InterCapturar.java
+++ src/controladores/InterCapturar.java
@@ -1,11 +1,21 @@
 package controladores;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class InterCapturar {
+public class InterCapturar implements Initializable{
     
     @FXML
     private Button btnGenerarPokemon;
@@ -19,7 +29,7 @@ public class InterCapturar {
     @FXML
     private Image imgPokemon;
 
-    
+
     public void GenerarPokemon(ActionEvent event){
 
 
@@ -33,5 +43,11 @@ public class InterCapturar {
     public void VolverInicio(ActionEvent event){
 
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    
+        
     }
 }
