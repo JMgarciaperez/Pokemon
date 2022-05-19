f685d01a0f2da900d1580185f8c20c39a851195c
JoseAndresCesur <98944622+JoseAndresCesur@users.noreply.github.com>
2022-05-19 12:10:15 +0200

    interfaz clase Combate

--- src/controladores/InterCombate.java
+++ src/controladores/InterCombate.java
@@ -1,5 +1,73 @@
 package controladores;
 
-public class InterCombate {
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class InterCombate implements Initializable{
+
+    @FXML
+    private Button btnAtaque1;
+
+    @FXML
+    private Button btnAtaque2;
+
+    @FXML
+    private Button btnAtaque3;
+
+    @FXML
+    private Button btnAtaque4;
+
+    @FXML
+    private Button btnRetirarse;
+
+    @FXML
+    private Text textRecuadro;
+
+    @FXML
+    private Text textNombrePoRival;
+
+    @FXML
+    private Text textNivelRival;
+
+    @FXML
+    private Text textMiNivel;
+
+    @FXML
+    private Text textNombrePo;
+
+    @FXML
+    private Text textMiVidalActual;
+
+    @FXML
+    private Text textMiVidaMax;
+
+    @FXML
+    private Text textVidaActualRi;
+
+    @FXML
+    private Text textVidaMaxRi;
+
+
+    @FXML
+    private ImageView imgMiPokemon;
+
+    @FXML
+    private ImageView imgPokemonRival;
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    } 
 }
