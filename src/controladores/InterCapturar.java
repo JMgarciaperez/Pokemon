8eda99f40a52be6f60405a597cd463af16546d89
JoseAndresCesur <98944622+JoseAndresCesur@users.noreply.github.com>
2022-05-19 09:47:07 +0200

    paso a la interfaz capturar

--- src/controladores/InterCapturar.java
+++ src/controladores/InterCapturar.java
@@ -1,5 +1,37 @@
 package controladores;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
 public class InterCapturar {
     
+    @FXML
+    private Button btnGenerarPokemon;
+
+    @FXML
+    private Button btnLanzarPokeball;
+
+    @FXML
+    private Button btnVolverInicio;
+
+    @FXML
+    private Image imgPokemon;
+
+    
+    public void GenerarPokemon(ActionEvent event){
+
+
+    }
+
+    public void LanzarPokeball(ActionEvent event){
+
+
+    }
+
+    public void VolverInicio(ActionEvent event){
+
+
+    }
 }
--- src/controladores/InterElegir.java
+++ src/controladores/InterElegir.java
@@ -1,12 +1,19 @@
 package controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class InterElegir implements Initializable {
 
@@ -19,9 +26,16 @@ public class InterElegir implements Initializable {
     private Button btnCombate;
 
     @FXML
-    private void capturarPokemon(ActionEvent event){
-        
-
+    private void capturarPokemon(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/menuCaptura.FXML"));
+        Parent root = loader.load();
+        InterCapturar controller = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
+        stage.close();
     }
 
     @FXML
